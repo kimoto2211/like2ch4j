9a1903b405087f67cc706e219b80b4b769d1354e
spc@spc <spc@spc>
2015-04-24 19:30:55 +0900

    a

--- src/like2ch4j/main/ThreadRead.java
+++ src/like2ch4j/main/ThreadRead.java
@@ -4,11 +4,13 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ThreadRead extends ThreadConnection {
 	public ThreadRead() {
 		super();
-		this.dir("/bbs/rawmode.cgi/");
+		this.dir("/bbs/read.cgi/");
 	}
 
 	public String read() throws Exception {
@@ -35,19 +37,14 @@ public class ThreadRead extends ThreadConnection {
 		br.close();
 		conn.disconnect();
 
-		StringBuilder tmp = new StringBuilder();
-		for (String b : parse(httpSource.toString())) {
-			tmp.append(b + "\n");
-		}
-
-		return tmp.toString();
+		return parse(httpSource.toString());
 
 	}
 
 	public static void shitarabaTest() {
 		try {
 			System.out.println(((ThreadRead) new ThreadRead()
-					.threadId("1426157209").board("internet/21265")
+					.threadId("1428764648").board("internet/21265")
 					.host("jbbs.shitaraba.net")).read());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -55,9 +52,42 @@ public class ThreadRead extends ThreadConnection {
 		}
 	}
 
-	public String[] parse(String str) {
-		String[] strs = str.split("<>");
-		return strs;
+	public String parse(String str) {
+		Pattern number = Pattern.compile("[0-9]+</a>");
+		Pattern mess = Pattern.compile("<dd> .*</br>");
+		String tmp = join(str, "<dt>", "\n");
+		tmp = join(tmp, "<!DOCTYPE.*\n", "");
+		tmp = join(tmp, "</dl>.*</html>", "");
+		String[] reses = tmp.split("\n");
+		StringBuilder[] formattedReses = new StringBuilder[reses.length];
+		StringBuilder sb = new StringBuilder();
 
+		for (int i = 0; i < reses.length; i++) {
+			System.out.print(gr(number, reses[i]) + ":" + gr(mess, reses[i])
+					+ "\n");
+		}
+
+		// tmp = join(tmp, "<a href=\"" + this.dir + this.board + "/"
+		// + this.threadId + "/[0-9]*\".*[a-z]*\">", "");
+		// tmp = join(tmp, "<br>", "\n");
+		// tmp = join(tmp, "<dd> ", "\n");
+		return "";
+	}
+
+	public String gr(Pattern p, String str) {
+		Matcher m = p.matcher(str);
+		if (m.find()) {
+			return m.group(0);
+		} else {
+			return "";
+		}
+	}
+
+	public String join(String str, String tar, String se) {
+		StringBuilder sb = new StringBuilder();
+		for (String s : str.split(tar)) {
+			sb.append(s + se);
+		}
+		return sb.toString();
 	}
 }
